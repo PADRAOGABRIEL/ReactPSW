79d3e84573b3f54daf14a10b1b5eb7f8e7d3de82
gabrielrodrig0 <gabrielrodrigonaga@gmail.com>
2023-09-07 17:39:54 -0300

    correção de nomes dos arquivos jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Header from "./components/header/header"
 import CreatePost from "./pages/criacaoPostagem/CreatePost"
+import inicial from "./pages/inicial/Home"
 function App() {
 
   return (
     <>
-      <CreatePost/>
-      
+      {/* <CreatePost/> */}
+      <inicial/>
     </>
   )
 }
